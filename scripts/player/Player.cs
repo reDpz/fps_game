e338a7c2c015abe439d61c6d1989ea11cbd484d1
redpz <sv_redpz@outlook.com>
2024-09-03 16:33:15 +0100

    basic character controller

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -1,63 +1,146 @@
+// vim:fileencoding=utf-8:foldmethod=marker
 using Godot;
 using System;
 using System.Runtime.CompilerServices;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	// : Properties {{{
 
+	// : Helper Values {{{
+	// 89 degrees, this is used so i'm not running math.f to degrees every frame
+	private const float X_ROTATION_LOCK_RAD = 1.55334f;
+	// : }}}
+
+	public const float speed = 5.0f;
+	public const float jump_velocity = 4.5f;
+
+	// : Input Properties {{{
+	public float mouse_sensitivity = 0.003f;
+	// : }}}
+
+	// : Physics Properties {{{
+	// setting a default, only to ensure that we dont run into null
+	public Vector3 gravity = Vector3.Zero;
 	private Vector3 velocity = Vector3.Zero;
 	private Vector3 input_vector = Vector3.Zero;
 	private Vector3 world_vector = Vector3.Zero;
+	// : }}}
 
-	public override void _PhysicsProcess(double delta)
+	// : Node Properties {{{
+	private Node3D head;
+	private Camera3D camera;
+	// : }}}
+
+	// : }}}
+
+
+	public override void _Ready()
 	{
+		// it should be kept in this layout in the project
+		head = GetNode<Node3D>("./head");
+		camera = GetNode<Camera3D>("./head/camera");
+
+		// lock mouse
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// : Camera Motions {{{
+		if (@event is InputEventMouseMotion mouse_motion)
+		{
+			mouse_motion.Relative *= mouse_sensitivity;
+			if (mouse_motion.Relative.X != 0.0)
+			{
+				this.RotateY(-mouse_motion.Relative.X);
+			}
+
+			if (mouse_motion.Relative.Y != 0.0)
+			{
+				head.RotateX(-mouse_motion.Relative.Y);
+
+				// lock the rotation on X
+				Vector3 rotation = new Vector3(
+						Math.Clamp(head.Rotation.X, -X_ROTATION_LOCK_RAD, X_ROTATION_LOCK_RAD),
+						head.Rotation.Y,
+						head.Rotation.Z
+						);
+				head.Rotation = rotation;
+
+			}
+
+
+		}
+
+		// : }}}
+	}
+
+	public override void _Process(double delta)
+	{
+		// update all values
+		gravity = GetGravity();
 		float felta = (float)delta;
-		Vector3 velocity = Velocity;
+
+		Vector2 input_dir = Input.GetVector("strafe_left", "strafe_right", "forward", "backward");
+		input_vector.X = input_dir.X;
+		input_vector.Z = input_dir.Y;
+		world_vector = Transform.Basis * input_vector;
 
 		// Add the gravity.
-		if (!IsOnFloor())
+		if (IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			GroundPhysics(felta);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			AirPhysics(felta);
 		}
+
+		CommonPhysics(felta);
+
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
 
 	// these functions are only called from Process and PhysicsProcess
+	// : Physics {{{
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public void GroundPhysics(float delta)
 	{
+		// attempt to jump
+		if (Input.IsActionPressed("jump"))
+		{
+			velocity.Y = jump_velocity;
+			return;
+		}
+
+		if (input_vector != Vector3.Zero)
+		{
+			velocity.X = world_vector.X * speed;
+			velocity.Z = world_vector.Z * speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
+		}
+
 
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public void AirPhysics(float delta)
 	{
-
+		// apply gravity
+		velocity += gravity * delta;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -65,4 +148,6 @@ public partial class Player : CharacterBody3D
 	{
 
 	}
+	// : }}}
+
 }
