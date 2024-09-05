60805f9acf481af55af324237f246b9e22d4c9ab
redpz <sv_redpz@outlook.com>
2024-09-05 16:53:05 +0100

    Added bot with physics interp

--- scripts/player/bot/plaza_bot.cs
+++ scripts/player/bot/plaza_bot.cs
@@ -5,39 +5,40 @@ public partial class plaza_bot : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
+	public Vector3 gravity = -Vector3.Up;
+	public float direction_change_timer = 0.5f;
+	public float x_movement = 1.0f;
+	public Random rand = new Random();
+
+	public override void _Ready()
+	{
+		gravity = GetGravity();
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-
+		float felta = (float)delta;
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			velocity += gravity * felta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+		Vector3 direction = (Transform.Basis * new Vector3(x_movement, 0.0f, 0.0f)).Normalized();
+		velocity.X = direction.X * Speed;
+		velocity.Z = direction.Z * Speed;
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		// consider the timer
+		direction_change_timer -= felta;
+		if (direction_change_timer <= 0 || IsOnWall())
+		{
+			// change direction
+			x_movement *= -1.0f;
+			direction_change_timer = 0.2f + (float)(rand.NextDouble());
+		}
 	}
 }
